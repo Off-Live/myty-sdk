4be7573610aa3c4461a82f7ea049c770e7f75200
jay <jay@off.live>
2023-03-31 14:20:46 +0900

    feat: Add Mocap4Face MotionSource

--- unity/Assets/Editor/MYTYSelectMotionSource.cs
+++ unity/Assets/Editor/MYTYSelectMotionSource.cs
@@ -57,5 +57,53 @@ namespace Editor
                 Debug.LogError("Failed to instantiate prefab: " + prefabPath);
             }
         }
+
+        [MenuItem("MYTY SDK/Select MotionSource/Mocap4Face")]
+        private static void AddMocap4Face()
+        {
+            var prefabPath = "Packages/com.offlive.myty.myty-sdk/Prefabs/Mocap4Face.prefab";
+
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+            if (prefab == null)
+            {
+                Debug.LogError("Prefab not found at path: " + prefabPath);
+                return;
+            }
+
+            GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+
+            if (instance != null)
+            {
+                instance.name = prefab.name;
+                PrefabUtility.UnpackPrefabInstance(instance, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+
+                instance.transform.position = Vector3.zero;
+
+                SceneManager.MoveGameObjectToScene(instance, SceneManager.GetActiveScene());
+
+                var motionSource = Object.FindObjectOfType<MotionSource.MotionSource>();
+
+                var avatarManager = Object.FindObjectOfType<AvatarManager>();
+                var messageHandler = Object.FindObjectOfType<MessageHandler.MessageHandler>();
+
+                messageHandler.motionSource = motionSource;
+                avatarManager.motionSource = motionSource;
+
+                var motionTemplateMapper = Object.FindObjectOfType<MotionTemplateMapper>();
+
+                var arFacePlane = Object.FindObjectOfType<ARFacePlane>();
+
+                arFacePlane.motionTemplateMapper = motionTemplateMapper;
+
+                Selection.activeGameObject = instance;
+
+                EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), SceneManager.GetActiveScene().path);
+            }
+            else
+            {
+                Debug.LogError("Failed to instantiate prefab: " + prefabPath);
+            }
+        }
     }
 }
